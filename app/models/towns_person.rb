f6d3895db5a180f3ac469b7e219629cea55e77de
Steven Nunez <steven.nunez@gmail.com>
2014-06-10 14:36:57 -0400

    Create models, migrations, and seed file

---
+++ app/models/towns_person.rb
@@ -0,0 +1,2 @@
+class TownsPerson < ActiveRecord::Base
+end
--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,4 @@ ActiveRecord::Base.establish_connection(
   adapter: 'postgresql',
   database: 'mafia'
 )
+Dir[File.join(__dir__, "../app/models", "*.rb")].each {|f| require f}

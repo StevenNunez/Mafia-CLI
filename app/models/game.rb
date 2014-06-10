deea1614146d92cacd1c0d9373f0681be328b25f
Steven Nunez <steven.nunez@gmail.com>
2014-06-10 15:16:41 -0400

    Add After create logic

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,11 @@ class Game < ActiveRecord::Base
   validates :label, presence: true
   has_many :towns_people
   has_many :students, through: :towns_people
+
+  after_create :populate_townspeople
+
+  def populate_townspeople
+    Student.join_game(self)
+    TownsPerson.mafiaize!(self)
+  end
 end

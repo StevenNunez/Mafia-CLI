e9eda7c794ac45ec7921ac2ed7a2703442228bd4
Steven Nunez <steven.nunez@gmail.com>
2014-06-10 17:07:33 -0400

    Finish Runner

--- app/models/game.rb
+++ app/models/game.rb
@@ -10,16 +10,53 @@ class Game < ActiveRecord::Base
     TownsPerson.mafiaize!(self)
   end
 
+  def over?
+    mafia_win? || mafia_lose?
+  end
+
+  def mafia_win?
+    mafia.count >= good_folks.count
+  end
+
+  def mafia_lose?
+    mafia.count.zero?
+  end
+
   def mafia_kill
-    good_folks.sample.destroy
+    dead = good_folks.sample.destroy
+    dead.student.name
   end
 
   def mob_kill(name)
+    # raise exception if student not found
     chop_block = Student.find_by(name: name)
-    TownsPerson.find_by(game: self, student: chop_block).destroy
+
+    towns_person = TownsPerson.find_by(game: self, student: chop_block)
+    if towns_person
+      towns_person.destroy.mafia?
+    else
+      raise "#{name} is already DEAD!!!!"
+    end
   end
 
   def good_folks
     self.towns_people.where(mafia: false)
   end
+
+  def mafia
+    self.towns_people.where(mafia: true)
+  end
+
+  def mafia_members
+    Student.joins(:towns_people).where(towns_people: {mafia: true, game: self})
+  end
+
+  def survivors
+    self.students.pluck(:name)  
+  end
 end
+
+# If mafia.count == good_folks.count Mafia wins show mafia
+# If mafia.count.zero? Good Folks win, YEY YOU KILLED THE MAFIA
+
+

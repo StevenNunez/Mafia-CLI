dedccb5cb7092c96d9a4ede5c12fc1e3305db49c
Steven Nunez <steven.nunez@gmail.com>
2014-06-10 15:40:16 -0400

    Kill people!

--- app/models/game.rb
+++ app/models/game.rb
@@ -9,4 +9,17 @@ class Game < ActiveRecord::Base
     Student.join_game(self)
     TownsPerson.mafiaize!(self)
   end
+
+  def mafia_kill
+    good_folks.sample.destroy
+  end
+
+  def mob_kill(name)
+    chop_block = Student.find_by(name: name)
+    TownsPerson.find_by(game: self, student: chop_block).destroy
+  end
+
+  def good_folks
+    self.towns_people.where(mafia: false)
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,10 @@ class Student < ActiveRecord::Base
   validates :name, presence: true
   has_many :towns_people
   has_many :games, through: :towns_people
+
+  def self.join_game(game)
+    self.all.each do |s|
+      s.games << game
+    end
+  end
 end

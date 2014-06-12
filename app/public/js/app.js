51b83145d7ab9d188160c068c8e499e5040b22c1
Steven Nunez <steven.nunez@gmail.com>
2014-06-12 00:00:26 -0400

    First pass at jQuery Mafia

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -2,17 +2,17 @@
 $(function(){
   if (window.location.pathname === '/'){
     $('.new-game-submit').click(function(event){
-        event.preventDefault();
-        var labelText = $('.new-game-label').text();
-        $.post('/games', {label: labelText}, function(){
-          window.location = '/play';
-        })
+      event.preventDefault();
+      var labelText = $('.new-game-label').val();
+      $.post('/games', {label: labelText}, function(){
+        window.location = '/play';
+      })
     });
 
     $.getJSON('games.json', function(games){
       games.forEach(function(game){
-        var template_string = "<li data-id='<%= item.id %>'><a href='#'><%= item.label %></a></li>"
-        var template = _.template(template_string);
+        var templateString = "<li data-id='<%= item.id %>'><a href='#'><%= item.label %></a></li>"
+        var template = _.template(templateString);
         $('#existing-games').append(template({item: game}));
       });
 
@@ -26,6 +26,43 @@ $(function(){
     });
   }
   if (window.location.pathname === '/play') {
-    console.log("Playing a game");
+    $('.sleep').click(function(e){
+        e.preventDefault();
+        $.post('/sleep.json', {}, function(data){
+          $('.sleep').attr("disabled", "disabled");
+          $('.notice').text(data.name + " was killed last night");
+          $('.towns-people li:contains("'+ data.name + '")').slideUp();
+        })
+    })
+
+    $.getJSON('/game.json', function(data){
+      data.townsPeople.forEach(function(townsPerson){
+        var templateString = "<li><a href='#'><%= item.name %></a></li>"
+        var template = _.template(templateString);
+        $('.towns-people').append(template({item: townsPerson}));
+      });
+      $('#game-label').text(data.label);
+      $('.towns-people li a').hover(function(){
+        $(this).addClass('shake shake-little');
+      }, function(){
+        $(this).removeClass('shake shake-little');
+      });
+
+      $('.towns-people li').on('click', function(event){
+        $('.sleep').removeAttr("disabled");
+        event.preventDefault();
+        var $dead = $(this);
+        var deadName = $dead.text();
+        $dead.slideUp();
+
+        $.post('/kill.json', { dead: deadName }, function(mafiaStatus){
+          if(mafiaStatus === true){
+            $('.notice').text(deadName + " was Mafia!");
+          } else {
+            $('.notice').text(deadName + " was NOT Mafia!");
+          }
+        });
+      });
+    });
   }
 })
--- app/server.rb
+++ app/server.rb
@@ -20,12 +20,29 @@ class MafiaApp < Sinatra::Base
   post '/games' do
     game = Game.create(label: params[:label])
     session[:game_id] = game.id
+    201
+  end
+
+  get '/game.json' do
+    content_type :json
+    game = Game.find(session[:game_id])
+    {
+      label: game.label,
+      townsPeople: game.students
+    }.to_json
   end
 
   post '/join/:id' do
     game = Game.find(params[:id])
     session[:game_id] = game.id
     201
+  end
+
+  post '/kill.json' do
+    content_type :json
+    game = Game.find(session[:game_id])
+    mafia_status = game.mob_kill(params[:dead])
+    mafia_status.to_json
   end
 
   post '/sleep.json' do
